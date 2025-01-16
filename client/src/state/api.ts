407cb25ebca03b603afbd82d1610570df2cfb5ea
zeff <jzeffsomera@gmail.com>
2025-01-16 14:11:07 +0800

    add global.d.ts config

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -11,9 +11,11 @@ const customBaseQuery = async (
   const baseQuery = fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     prepareHeaders: async (headers) => {
-      const token = await window.Clerk?.session?.getToken();
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
+      if (typeof window !== "undefined" && window.Clerk?.session) {
+        const token = await window.Clerk.session.getToken();
+        if (token) {
+          headers.set("Authorization", `Bearer ${token}`);
+        }
       }
       return headers;
     },
